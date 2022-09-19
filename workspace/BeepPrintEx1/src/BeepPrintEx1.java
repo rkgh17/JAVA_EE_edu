b6264c198e3efea8314fab50ece130b581892672
황지훈 <rkgh17@gmail.com>
2022-09-19 10:49:19 +0900

    day28_1t

--- workspace/BeepPrintEx1/src/BeepPrintEx1.java
+++ workspace/BeepPrintEx1/src/BeepPrintEx1.java
@@ -6,20 +6,12 @@ public class BeepPrintEx1 {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		for (int i = 0; i < 5; i++) {
 			toolkit.beep();
-			try {
-				Thread.sleep(500);
-			} catch (Exception e) {
-			}
+			try {Thread.sleep(500);} catch (Exception e) {}
 		}
 
 		for (int i = 0; i < 5; i++) {
 			System.out.println("띵");
-			try {
-				Thread.sleep(500);
-			} catch (Exception e) {
-			}
+			try {Thread.sleep(500);} catch (Exception e) {}
 		}
-
 	}
-
 }
