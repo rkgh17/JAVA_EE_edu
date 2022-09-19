42766dcdc268f359025ce03c9c6bc193a11eb12e
황지훈 <rkgh17@gmail.com>
2022-09-19 11:17:36 +0900

    day28_3t

--- workspace/BeepPrintEx2/src/BeepPrintEx2.java
+++ workspace/BeepPrintEx2/src/BeepPrintEx2.java
@@ -4,7 +4,7 @@ public class BeepPrintEx2 {
 	public static void main(String[] args) {
 		Runnable beepTask = new BeepTask(); // 구현클래스의 새 인스턴스를 만듬
 		Thread thread = new Thread(beepTask); // 인스턴스를 Thread 클래스에 넣어준다
-		thread.start(); // start()호출
+		thread.start(); // start()호출 
 		
 		for (int i = 0; i < 5; i++) {
 			System.out.println("띵");
